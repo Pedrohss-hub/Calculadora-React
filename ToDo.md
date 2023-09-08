6759f231e787b7fb6fc6469b646a0acd8fdb37e3
Pedrohss-hub <pedrohss466@gmail.com>
2023-09-07 23:10:15 -0300

    Prog: Adição da funcionalidade de Porcentagem ao botão

--- ToDo.md
+++ ToDo.md
@@ -3,11 +3,15 @@ A fazeres:
 Visual: 
 - Diminuir o tamanho dos operadores {}
 
+-- Desenvolver um Design próprio {{}}
+
 Programático
 
 - Deixar zero como valor inicial {x}
 - Apenas 1 operador {x}
-- Btn Porcentagem(%) {}
-
+- Btn Porcentagem(%) {x}
 - Parênteses {}
+
 - Apagar um item anterior {}
+
+-- Organizar o Código {{}}
--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 const btns = document.querySelectorAll('.op')
 const visor = document.querySelector('.visor')
+let percentage = false
 visor.textContent = 0
 
 // Atualizar o Valor do Visor
 const updateVisor = (val) => {
-
-
+        if(val == '%'){
+            percentage = true
+        }
+        
             //Apagar todos os Valores
         if (val == 'AC') {
             visor.textContent = 0
             //Ativar Operadores
             btns.forEach((item)=>{
-                item.disabled = false
+                item.disabled = true
             })
 
+            //Inserção de Valores
         } else {
             
             //Desabilitando Operadores
@@ -21,32 +25,32 @@ const updateVisor = (val) => {
                 visor.textContent += val
                 btns.forEach((item) => {
                 console.log('desativado');
-                item.disabled = true;
+                item.disabled = false;
                 console.log(item)
-            });
+                });
 
             } else {
-            //Inserção de Valores
-            if((visor.value == 0 && val > 0 && visor.value != '0.') || (val == '0' && visor.value == '0')){
-                visor.textContent = val
-                //Ativar Operadores
-                btns.forEach((item)=>{
-                    item.disabled = false
-                })
+                //Adição Inicial de Números
+                if((visor.value == 0 && val > 0 && visor.value != '0.') || (val == '0' && visor.value == '0')){
+                    visor.textContent = val
+                    //Ativar Operadores
+                    btns.forEach((item)=>{
+                        item.disabled = false
+                    })
 
-            } else {
-                visor.textContent += val 
-                //Ativar os operadores
-                btns.forEach((item)=>{
-                    item.disabled = false
-                })
-
+                //Adição de Números
+                } else {
+                    visor.textContent += val 
+                    //Ativar os operadores
+                    btns.forEach((item)=>{
+                        item.disabled = false
+                    })
+                }
             }
-
         }
-    }
     
     
+    //Tranformando dados em Array
     visor.value = visor.textContent
     let visualArithmetic = visor.value.split('')
 
@@ -64,6 +68,20 @@ const updateVisor = (val) => {
 
 //Calcular o Valor
 const calculate = () => {
+    if (percentage){
+        percentage = visor.textContent.split('')
+        console.log(percentage)
+        percentage.forEach((item,index,array)=>{
+            if (item == '%') {
+                array[index] = '/100*'
+            }
+        })
+
+        visor.textContent = percentage.join('')
+        console.log(percentage)
+        percentage = false
+    }
     visor.textContent = eval(visor.textContent)
+    console.log(visor.textContent)
     visor.value = visor.textContent
 }
