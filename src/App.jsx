dc98a5e33f2c2fe336d9d7838e1fc158141974fa
Pedrohss-hub <pedrohss466@gmail.com>
2023-09-29 11:38:18 -0300

    Continuar no Pc

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import { useState } from 'react';
 import './App.css';
 
 function Keys(props) {
-  return <button className={`keys ${props.className}`}>{props.children}</button>
+  function updateDisplay() {
+    props.onButtonClick(props.children)
+    console.log(props)  
+  } 
+
+  return(
+    <button onClick={updateDisplay} className={`keys ${props.className}`}>{props.children}</button>
+    );
 }
 
 function App() {
+  const [values, setValues] = useState([])
+
+  function defineValue(value) {
+    setValues([...values, value])
+  }
+
   return(
   <div className="container-main">
-    <div className="display-box"></div>
+    <div className="display-box">{values}</div>
     <div className="keys-box">
-        <Keys className="specialKey2">(</Keys>
+        <Keys onButtonClick={defineValue} className="specialKey2">(</Keys>
         <Keys className="specialKey2">)</Keys>
         <Keys className="specialKey2">%</Keys>
         <Keys className="specialKey">AC</Keys>
@@ -21,7 +34,7 @@ function App() {
         <Keys>4</Keys>
         <Keys>5</Keys>
         <Keys>6</Keys>
-        <Keys className="specialKey">X</Keys>
+        <Keys className="specialKey">*</Keys>
         <Keys>1</Keys>
         <Keys>2</Keys>
         <Keys>3</Keys>
